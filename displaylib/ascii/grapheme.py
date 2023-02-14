f765afa634dd1bb888a6b0b2e0f86f1b08612eab
Floating-Int <knutolaihelgesen@gmail.com>
2023-02-14 20:13:27 +0100

    Initial commit

---
+++ displaylib/ascii/grapheme.py
@@ -0,0 +1,20 @@
+
+
+# -- database
+database = {
+    "/": "\\",
+    "(": ")"
+}
+
+# -- mirror database
+for key, value in tuple(database.items()):
+    database[value] = key
+
+def lookup(letter: str) -> str:
+    return database.get(letter, letter)
+
+def flip(line: str) -> str:
+    return "".join(database.get(letter, letter) for letter in line)[::-1]
+
+def mapflip(content: list[str]) -> list[str]:
+    return [flip(line) for line in content]
