c5b9360479c51afaf404fef54521ac1efac4b3e2
Floating-Int <knutolaihelgesen@gmail.com>
2023-03-08 21:10:19 +0100

    Added type hints and documentation

--- displaylib/template/node.py
+++ displaylib/template/node.py
@@ -1,6 +1,10 @@
 from typing_extensions import Self
+from typing import TYPE_CHECKING
 from ..math import Vec2
-from .types import Engine
+
+
+if TYPE_CHECKING:
+    from .engine import Engine
 
 
 class Node:
@@ -9,12 +13,12 @@ class Node:
     Automatically keeps track of alive Node(s) by reference.
     An Engine subclass may access it's nodes through the `nodes` class attribute
     """
-    __logical__ = True # logical means it won't have a position or existance in the "real world"
+    __logical__: bool = True # logical means it won't have a position or existance in the "real world"
 
-    root: Engine = None # set from a Engine subclass
-    nodes = {} # all nodes that are alive
-    _request_sort = False # requests Engine to sort
-    _queued_nodes = set() # uses <Node>.queue_free() to ask Engine to delete them
+    root: "Engine" = None # set from a Engine subclass
+    nodes: dict[int, Self] = {} # all nodes that are alive
+    _request_sort: bool = False # requests Engine to sort
+    _queued_nodes: set = set() # uses <Node>.queue_free() to ask Engine to delete them
 
     def __new__(cls: type[Self], *args, **kwargs) -> Self:
         instance = super().__new__(cls)
@@ -28,29 +32,54 @@ class Node:
             Node._request_sort = True # otherwise, depent on a `z_index` change
 
     def __repr__(self) -> str:
+        """Returns a default representation of the Node object
+
+        Returns:
+            str: node representation
+        """
         return f"{self.__class__.__name__}()"
     
     @property
     def name(self) -> str:
+        """Returns class name
+
+        Returns:
+            str: class name
+        """
         return self.__class__.__name__
 
+    def where(self, **attributes) -> Self:
+        for key, value in attributes.items():
+            setattr(self, key, value)
+        return self
+
     def _update(self, delta: float) -> None:
+        """Called every frame by the Engine class
+        
+        Override for custom functionality
+
+        Args:
+            delta (float): time since last frame
+        """
         return
     
     def queue_free(self) -> None:
+        """Tells the Engine to `delete` this <Node> after
+        every node has been called `_update` on
+        """
         Node._queued_nodes.add(self)
 
 
 class Node2D(Node):
     """Node2D class with transform attributes
     """
-    __logical__ = False
+    __logical__: bool = False
 
     def __init__(self, parent: Self | None = None, x: int = 0, y: int = 0, z_index: int = 0, force_sort: bool = True) -> None:
         self.parent = parent
         self.position = Vec2(x, y)
         self._z_index = z_index
-        self.visible = True
+        self.visible = True # only nodes on the 2D plane will have the option to be visible
         if force_sort: # if True, requests sort every frame a new node is created
             Node._request_sort = True # otherwise, depend on a `z_index` change
     
