46dee81bf85e5f0d4f555abca513c61a4a149e5b
Floating-Int <knutolaihelgesen@gmail.com>
2023-03-24 18:48:15 +0100

    Attatched new metaclass EngineMixinSortMeta to base Engine class

--- displaylib/template/engine.py
+++ displaylib/template/engine.py
@@ -3,30 +3,30 @@ from __future__ import annotations
 from .node import Node
 
 
-class EnginePriorityMeta(type):
-    """Engine metaclass for initializing `Engine` subclass before other `mixin` classes
+class EngineMixinSortMeta(type):
+    """Engine metaclass for initializing `Engine` subclass after other `mixin` classes
     """
     @staticmethod
     def _mixin_sort(base: type) -> bool:
-        return not issubclass(base, Engine)
+        return issubclass(base, Engine)
 
-    def __new__(cls, name: str, bases: tuple[type], dct: dict[str, object]):
-        sorted_bases = tuple(sorted(bases, key=EnginePriorityMeta._mixin_sort))
-        return super().__new__(cls, name, sorted_bases, dct)
+    def __new__(cls, name: str, bases: tuple[type], attrs: dict[str, object]):
+        sorted_bases = tuple(sorted(bases, key=EngineMixinSortMeta._mixin_sort))
+        return super().__new__(cls, name, sorted_bases, attrs)
 
 
-class Engine(metaclass=EnginePriorityMeta):
+class Engine(metaclass=EngineMixinSortMeta):
     """Engine base class
     NOTE: only one Engine instance should exist per script instance
     """
     tps: int = 4
     is_running: bool = False
 
-    def __new__(cls: type[Engine]) -> Engine:
-        """Sets `Node.root` when an Engine instance is initialized 
+    def __new__(cls: type[Engine], *args, **kwargs) -> Engine:
+        """Sets `Node.root` when an `Engine instance` is initialized 
 
         Args:
-            cls (type[Engine]): engine object to be root
+            cls (type[Engine]): engine object to be `root`
 
         Returns:
             Engine: the engine to be used in the program
@@ -35,7 +35,8 @@ class Engine(metaclass=EnginePriorityMeta):
         setattr(Node, "root", instance)
         return instance
 
-    def __init__(self) -> None: # default implementation
+    def __init__(self, *args, **kwargs) -> None: # default implementation
+        super(__class__, self).__init__(*args, **kwargs)
         self._on_start()
         self.is_running = True
         self._main_loop()
@@ -54,7 +55,7 @@ class Engine(metaclass=EnginePriorityMeta):
         def sort_fn(element: tuple[int, Node]):
             return element[1].z_index
         
-        delta = 1.0 / self.tps
+        delta = 1.0 / self.tps # TODO: implement default clock
         nodes = tuple(Node.nodes.values())
         while self.is_running:
             self._update(delta)
