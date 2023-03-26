00a00bb377e5c0c719ecf8fe8d345805d855a8b6
Floating-Int <knutolaihelgesen@gmail.com>
2023-03-26 19:04:54 +0200

    Added GitHub link and PyPI link

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
-from displaylib import __version__, __author__
+from displaylib import __doc__, __version__, __author__
 
 
 setup(
    name="displaylib",
    version=__version__,
-   description="A collection of frameworks used to display ASCII or Pygame graphics",
    author=__author__,
+   description="A collection of frameworks used to display ASCII or Pygame graphics",
+   long_description=__doc__,
+   url="https://github.com/Floating-Int/displaylib",
+   download_url="https://pypi.org/project/displaylib/",
    packages=[
       "displaylib",
       "displaylib.template",
