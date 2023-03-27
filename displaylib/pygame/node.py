5856052e611f8d2ecdb70bcfc41c0c51a7bf497c
Floating-Int <knutolaihelgesen@gmail.com>
2023-03-27 11:31:57 +0200

    Added documentation for hooks

--- displaylib/pygame/node.py
+++ displaylib/pygame/node.py
@@ -9,13 +9,27 @@ if TYPE_CHECKING:
 
 
 class PygameNode2D(Node2D):
-    """`PygameNode2D` for representing 2D nodes
+    """`PygameNode2D` with additional hooks from Node2D
+
+    Hooks:
+        `_input(self, event: pygame.event.Event) -> None`
+        `_render(self, surface: pygame.Surface) -> None`
     """
     def __init__(self, parent: Node | None = None, *, x: int = 0, y: int = 0, z_index: int = 0, force_sort: bool = True) -> None:
-        super().__init__(parent, x, y, z_index, force_sort)
+        super().__init__(parent, x, y, z_index=z_index, force_sort=force_sort)
     
     def _input(self, event: pygame.event.Event) -> None:
+        """Override for custom functionality
+
+        Args:
+            event (pygame.event.Event): event received
+        """
         ...
     
     def _render(self, surface: pygame.Surface) -> None:
+        """Override for custom functionality
+
+        Args:
+            surface (pygame.Surface): surface to render custom content onto
+        """
         ...
