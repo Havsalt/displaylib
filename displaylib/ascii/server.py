a966464ca4b0089375f93424546d602b20bb9b1e
Floating-Int <knutolaihelgesen@gmail.com>
2023-03-30 09:03:56 +0200

    Initial commit

---
+++ displaylib/ascii/server.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from ..template import BroadcastServer
+
+
+class ASCIIBroadcastServer(BroadcastServer):
+    """`ASCIIBroadcastServer` compatible with the `displaylib.ascii` structure
+    """
