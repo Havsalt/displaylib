eebc0fccf528b9b1dc224eac9be01d43859bb6dd
Floating-Int <knutolaihelgesen@gmail.com>
2023-03-31 08:58:16 +0200

    Added documentation

--- displaylib/ascii/camera.py
+++ displaylib/ascii/camera.py
@@ -11,6 +11,16 @@ if TYPE_CHECKING:
 
 class ASCIICamera(Node2D):
     """`ASCIICamera` for moving the viewport
+
+    Hooks:
+        - `_render(self, surface: ASCIISurface) -> None`
+        - `_on_screen_resize(self, size: Vec2i) -> None`
+    
+    ModeFlags for attribute `mode`:
+        - 1 `FIXED` (Default)
+        - 3 `CENTERED`
+        - 5 `INCLUDE_SIZE`
+        - 8 `CENTERED_AND_INCLUDE_SIZE` (`CENTERED + INCLUDE_SIZE`)
     """
     FIXED = 1
     CENTERED = 3
@@ -19,13 +29,22 @@ class ASCIICamera(Node2D):
     current: ASCIICamera
 
     def __init__(self, parent: Node | None = None, x: int = 0, y: int = 0, *, follow: bool = False, mode: int = FIXED) -> None:
-        self._z_index = 0 # required in <Node2D>.__init__
-        super().__init__(parent, x, y, -1, True) # TODO: move z_index into some config (the "-1" part)
+        super().__init__(parent, x, y, z_index=-1, force_sort=True) # TODO: move z_index into some config (the "-1" part)
         self.mode = mode # `centered` mode only has effect if `parent` != None
         self.follow = follow # whether to follow the `parent`
     
     def _render(self, surface: ASCIISurface) -> None:
+        """Override for custom functionality
+
+        Args:
+            surface (ASCIISurface): surface to blit onto
+        """
         ...
 
     def _on_screen_resize(self, size: Vec2i) -> None:
+        """Override for custom functionality
+
+        Args:
+            size (Vec2i): the new screen size
+        """
         ...
