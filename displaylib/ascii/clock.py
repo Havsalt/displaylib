1f567318c40a9834c87e76d3931de30dd647ff5b
Floating-Int <knutolaihelgesen@gmail.com>
2023-03-31 08:59:54 +0200

    Finally fixed the .tick() method

--- displaylib/ascii/clock.py
+++ displaylib/ascii/clock.py
@@ -4,14 +4,17 @@ import time
 
 
 class Clock:
+    """`Clock` that pauses the program with adjusted `deltatime` based on execution time of a timeframe
+    """
+    
     def __init__(self, tps: int) -> None:
         """Initilizes the clock
 
         Args:
             tps (int): ticks per second
         """
-        self._tps = tps
-        self._delta = 1.0 / self.tps
+        self.tps = tps
+        self._target_delta = 1.0 / self.tps
         self._last_tick = time.perf_counter()
     
     @property
@@ -21,7 +24,15 @@ class Clock:
     @tps.setter
     def tps(self, value: int) -> None:
         self._tps = value
-        self._delta = 1.0 / self._tps
+        self._target_delta = 1.0 / self._tps
+    
+    def get_delta(self) -> float:
+        """Returns the current deltatime since last clock tick
+
+        Returns:
+            float: time since last tick
+        """
+        return max(0.0, time.perf_counter() - self._last_tick)
     
     def tick(self) -> float:
         """Pauses the clock temporay to achieve the desired framerate (tps)
@@ -29,12 +40,11 @@ class Clock:
         Returns:
             float: delta time since last tick
         """
-        time.sleep(self._delta) # QUICKFIX
-        return self._delta
         now = time.perf_counter()
         diff = now - self._last_tick
-        remaining = self._delta - diff
+        remaining = self._target_delta - diff
         if remaining <= 0:
+            self._last_tick = now
             return 0.0
         time.sleep(remaining)
         self._last_tick = time.perf_counter()
