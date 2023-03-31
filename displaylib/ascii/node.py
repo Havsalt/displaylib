add7952b619fcffc784ee9b3a0cac834ed62dba1
Floating-Int <knutolaihelgesen@gmail.com>
2023-03-31 09:01:31 +0200

    Added documentation

--- displaylib/ascii/node.py
+++ displaylib/ascii/node.py
@@ -5,15 +5,29 @@ from typing import TYPE_CHECKING
 from ..template import Node2D
 
 if TYPE_CHECKING:
-    from ..math import Vec2
+    from ..math import Vec2i
     from .surface import ASCIISurface
 
 
 class ASCIINode2D(Node2D): # a variant of the Node2D
-    """`ASCIINode2D` for representing 2D nodes
+    """`ASCIINode2D` with additional hooks related to ascii mode functionality
+
+    Hooks:
+        - `_render(self, surface: ASCIISurface) -> None`
+        - `_on_screen_resize(self, size: Vec2i) -> None`
     """
     def _render(self, surface: ASCIISurface) -> None:
+        """Override for custom functionality
+
+        Args:
+            surface (ASCIISurface): surface to blit other surfaces onto
+        """
         ...
     
-    def _on_screen_resize(self, size: Vec2) -> None:
+    def _on_screen_resize(self, size: Vec2i) -> None:
+        """Override for custom functionality
+
+        Args:
+            size (Vec2i): new screen size
+        """
         ...
