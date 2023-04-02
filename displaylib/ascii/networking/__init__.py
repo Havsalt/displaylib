9cc91688b8e067b3a5238f0aa6f861d35e2472bc
Floating-Int <knutolaihelgesen@gmail.com>
2023-04-02 21:21:44 +0200

    Reflected the implementation of register_class

--- displaylib/ascii/networking/__init__.py
+++ displaylib/ascii/networking/__init__.py
@@ -4,15 +4,17 @@ Provies both `Client` and `Server` classes for integrating networking compatible
 """
 
 __all__ = [
+    "register_class",
     "serialize",
     "deserialize",
     "SerializeError",
+    "DeserializeError",
     "Client",
     "Server",
     "BroadcastServer"
 ]
 
-from ...template.networking import serialize, deserialize, SerializeError
+from ...template.networking import register_class, serialize, deserialize, SerializeError, DeserializeError
 from .client import ASCIIClient as Client
 from .server import ASCIIServer as Server
 from .broadcast_server import ASCIIBroadcastServer as BroadcastServer
