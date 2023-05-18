56b4e72bdde18914eefb77f88e6e449067af215f
Floating-Int <knutolaihelgesen@gmail.com>
2023-05-18 21:58:22 +0200

    Using content from README.md as `long_description`

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from setuptools import setup
 from displaylib import __doc__, __version__, __author__
 
 
+with open("README.md", "r") as markdown_file:
+   markdown_content = markdown_file.read()
+
 setup(
    name="displaylib",
    version=__version__,
    author=__author__,
-   description="A collection of frameworks used to display ASCII or Pygame graphics",
-   long_description=__doc__,
+   description="A collection of frameworks used to display ASCII or Pygame graphics in an infinite world",
+   long_description=markdown_content,
+   long_description_content_type="text/markdown",
    url="https://github.com/Floating-Int/displaylib",
    download_url="https://pypi.org/project/displaylib/",
+   python_requires=">=3.10",
    packages=[
       "displaylib",
       "displaylib.template",
