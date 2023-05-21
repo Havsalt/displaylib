4cd91cd9eb09112abefbc98175c3e6de1bffb984
Floating-Int <knutolaihelgesen@gmail.com>
2023-05-21 18:00:23 +0200

    Initial commit

---
+++ displaylib/template/node2d.py
@@ -0,0 +1,12 @@
+from .node import Node
+from .transform import Transform2D
+
+
+class Node2D(Transform2D, Node):
+    """`Node2D` class with transform attributes
+
+    Components:
+        `Transform2D`: provides transform attributes
+    """
+    def __init__(self, parent: Node | None = None, *, x: int | float = 0, y: int | float = 0, force_sort: bool = True) -> None:
+        super().__init__(parent, force_sort=force_sort)
---
+++ displaylib/template/transform.py
@@ -0,0 +1,70 @@
+from typing import TypeVar
+
+from ..math import Vec2
+from .node import Node
+
+Self = TypeVar("Self")
+
+
+class Transform2D: # Component (mixin class)
+    """`Transform2D` mixin class for adding position, rotation and visibility to a node class
+    """
+    position: Vec2
+    rotation: float
+
+    def __new__(cls: type[Self], *args, x: int | float = 0, y: int | float = 0, force_sort: bool = True, **kwargs) -> Self:
+        instance = super().__new__(cls, *args, force_sort=force_sort, **kwargs) # `force_sort` is passed to Node eventually
+        setattr(instance, "position",  Vec2(x, y))
+        setattr(instance, "rotation", 0.0)
+        setattr(instance, "_visible", True) # only nodes with Transform2D will have the option to be visible
+        if force_sort: # if True, requests sort every frame a new node is created
+            Node._request_process_priority_sort = True # otherwise, depend on a `process_priority` change
+        return instance
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}({self.position.x}, {self.position.y})"
+    
+    @property
+    def global_position(self) -> Vec2:
+        position = self.position
+        parent = self.parent
+        while parent is not None and isinstance(parent, Transform2D):
+            position += parent.position.rotated(parent.rotation)
+            parent = parent.parent
+        return position
+    
+    @global_position.setter
+    def global_position(self, position: Vec2) -> None:
+        diff = position - self.global_position
+        self.position += diff
+    
+    @property
+    def global_rotation(self) -> float:
+        rotation = self.rotation
+        parent = self.parent
+        while parent is not None and isinstance(parent, Transform2D):
+            rotation += parent.rotation
+            parent = parent.parent
+        return rotation
+
+    @global_rotation.setter
+    def global_rotation(self, rotation: float) -> None:
+        diff = rotation - self.global_rotation
+        self.rotation += diff
+    
+    @property
+    def visible(self) -> bool: # global visibility
+        if not self._visible:
+            return False
+        parent = self.parent
+        while parent != None:
+            if not isinstance(parent, Transform2D):
+                return True
+            if not parent._visible:
+                return False
+            parent = parent.parent
+        return True
+
+    @visible.setter
+    def visible(self, value: bool) -> None: # local visibility
+        self._visible = value
