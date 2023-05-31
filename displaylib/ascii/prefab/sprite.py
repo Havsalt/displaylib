157b4219d2a3fd82cb4ce30683f55bfd4b13d3a3
Floating-Int <knutolaihelgesen@gmail.com>
2023-05-31 13:08:00 +0200

    Using pull and made __init__ interface

--- displaylib/ascii/prefab/sprite.py
+++ displaylib/ascii/prefab/sprite.py
@@ -1,16 +1,29 @@
 from __future__ import annotations
 
+from typing import TypeVar
+
 from ...math import Vec2i
+from ...util import pull
+from ...template import Node
 from ..node import ASCIINode2D
 from ..texture import Texture
 
+Self = TypeVar("Self")
 
+
+@pull("texture")
 class ASCIISprite(Texture, ASCIINode2D):
     """Prefabricated `ASCIISprite`
 
     Components:
         `Texture`: allows the node to be displayed
     """
+    texture: list[list[str]]
+
+    def __init__(self, parent: Node | None = None, *, x: int | float = 0, y: int | float = 0, texture: list[list[str]] = [], z_index: int = 0, force_sort: bool = True) -> None: # `z_index` pulled in `Texture`
+        super().__init__(parent, x=x, y=y, force_sort=force_sort)
+        self.z_index = z_index
+        self.texture = texture
     
     def size(self) -> Vec2i:
         """Returns the width and height of `.texture` as a vector
