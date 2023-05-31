9630fd60c474cc7500dd79614eede2569fe1012d
Floating-Int <knutolaihelgesen@gmail.com>
2023-05-31 12:56:29 +0200

    Added ClassVar type hint

--- displaylib/ascii/texture.py
+++ displaylib/ascii/texture.py
@@ -1,22 +1,25 @@
 from __future__ import annotations
 
-from typing import ClassVar
+from typing import TypeVar, ClassVar
 
 from ..template import Transform2D
+
+Self = TypeVar("Self")
 
 
 class Texture: # Component (mixin class)
     """`Texture` mixin class for adding ASCII graphics to a node class
 
     Requires Components:
-        `Transform2D`: uses position and rotation to place the texture
+        - `Transform2D`: uses position and rotation to place the texture
     """
     texture: list[list[str]]
     visible: bool
-    _instances: list[Texture] = [] # nodes with Texture component
+    _instances: ClassVar[list[Texture]] = [] # references to nodes with Texture component
     _request_z_index_sort: ClassVar[bool] = False # requests Engine to sort
+    _request_instance_removal: ClassVar[bool] = False # requests Engine to decrement ref count to Texture instance
 
-    def __new__(cls, *args, z_index: int = 0, force_sort: bool = True, **kwargs):
+    def __new__(cls: type[Self], *args, z_index: int = 0, force_sort: bool = True, **kwargs) -> Self:
         instance = super().__new__(cls, *args, force_sort=force_sort, **kwargs) # `force_sort` is passed to Node eventually
         if not isinstance(instance, Transform2D):
             raise TypeError(f"in class '{instance.__class__.__qualname__}': mixin class '{__class__.__qualname__}' requires to be used in combination with a node class deriving from 'Transform2D'")
