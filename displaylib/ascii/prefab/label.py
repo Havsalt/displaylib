2d23977cf6f063f822b78a89c7696273be3cd3d1
Floating-Int <knutolaihelgesen@gmail.com>
2023-05-31 13:09:00 +0200

    Using pull and made __init__ interface

--- displaylib/ascii/prefab/label.py
+++ displaylib/ascii/prefab/label.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from ...util import pull
 from ..node import ASCIINode2D
 from ..texture import Texture
 
@@ -9,17 +10,16 @@ if TYPE_CHECKING:
     from ...template import Node
 
 
+@pull("text")
 class ASCIILabel(Texture, ASCIINode2D):
     """Prefabricated `Label` node where a new line is created for each `\\n`
     
     Components:
         `Texture`: allows the node to be shown
     """
-    def __new__(cls, *args, text: str = "", **kwargs): # pulling: `text`
-        return super().__new__(cls, *args, **kwargs)
-
     def __init__(self, parent: Node | None = None, *, x: int = 0, y: int = 0, text: str = "", z_index: int = 0, force_sort: bool = True) -> None:
         super().__init__(parent, x=x, y=y, force_sort=force_sort)
+        self.z_index = z_index
         self.text = text
     
     @property
