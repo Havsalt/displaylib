855c36350bde30e87842d9a28ff01f2ae4247578
Floating-Int <knutolaihelgesen@gmail.com>
2023-05-31 13:08:48 +0200

    Using pull

--- displaylib/ascii/prefab/line.py
+++ displaylib/ascii/prefab/line.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, ClassVar
 
 from ...math import Vec2
+from ...util import pull
 from ..node import ASCIINode2D
 from ..texture import Texture
 
@@ -10,20 +11,20 @@ if TYPE_CHECKING:
     from ...template import Node
 
 
+@pull("texture")
 class ASCIIPoint2D(Texture, ASCIINode2D):
     """Thin wrapper around `ASCIINode2D` capable of displaying a single point
     
     Components:
         `Texture`: allows the node to be shown
     """
-    def __new__(cls, *args, texture: str = "#", **kwargs): # pulling: `texture`
-        return super().__new__(*args, **kwargs)
-
-    def __init__(self, parent: Node | None = None, x: int = 0, y: int = 0, *, texture: str = "#", force_sort: bool = True) -> None:
+    def __init__(self, parent: Node | None = None, x: int = 0, y: int = 0, *, texture: str = "#", z_index: int = 0, force_sort: bool = True) -> None:
         super().__init__(parent, x=x, y=y, force_sort=force_sort)
         self.texture = [[texture]]
+        self.z_index = z_index
 
 
+@pull("start", "end", "texture")
 class ASCIILine(ASCIINode2D):
     """Prefabricated `Line` node
 
@@ -31,40 +32,45 @@ class ASCIILine(ASCIINode2D):
     """
     texture_default: ClassVar[str] = "#" # only used when creating a line node
 
-    def __new__(cls, *args, start: Vec2 = Vec2(0, 0), end: Vec2 = Vec2(0, 0), texture: str | None = None, **kwargs): # pulling: `start`, `end`, `texture`
-        return super().__new__(*args, **kwargs)
-
-    def __init__(self, parent: Node | None = None, x: int = 0, y: int = 0, *, start: Vec2 = Vec2(0, 0), end: Vec2 = Vec2(0, 0), texture: str | None = None, z_index: int = 0, force_sort: bool = True) -> None:
+    def __init__(self, parent: Node | None = None, x: int = 0, y: int = 0, *, start: Vec2 = Vec2(0, 0), end: Vec2 = Vec2(0, 0), texture: str = texture_default, z_index: int = 0, force_sort: bool = True) -> None:
         super().__init__(parent, x=x, y=y, force_sort=force_sort)
+        self.z_index = z_index
+        self.force_sort = force_sort
         self.start = start
         self.end = end
-        self.texture = texture or self.texture_default
-        self.z_index = z_index
+        self.texture = texture
         self.points: list[ASCIIPoint2D] = [
-            ASCIIPoint2D(self, z_index=self.z_index, texture=self.texture).where(position=start),
-            ASCIIPoint2D(self, z_index=self.z_index, texture=self.texture).where(position=end)
+            ASCIIPoint2D(self, texture=self.texture, z_index=self.z_index, force_sort=force_sort).where(position=start),
+            ASCIIPoint2D(self, texture=self.texture, z_index=self.z_index, force_sort=force_sort).where(position=end)
         ]
+        self._update(0) # simulate initial frame locally
 
     def _update(self, delta: float) -> None:
-        # -- clear points
+        # clear points
         for point in self.points:
             point.queue_free()
         self.points.clear()
-
+        
         if not self.visible:
             return
-        # -- create points along the current/new line
+
+        # creating new ends of line
+        self.points: list[ASCIIPoint2D] = [
+            ASCIIPoint2D(self, texture=self.texture, z_index=self.z_index, force_sort=self.force_sort).where(position=self.start),
+            ASCIIPoint2D(self, texture=self.texture, z_index=self.z_index, force_sort=self.force_sort).where(position=self.end)
+        ]
+        # create points along the current/new line
         diff = (self.end - self.start)
         direction = diff.normalized()
-        length = diff.length() # length of difference
+        length = diff.length() # distance determined by difference
         steps = round(length)
         for idx in range(steps):
             position = self.start + (direction * idx)
-            point = ASCIIPoint2D(self, x=int(position.x), y=int(position.y), texture=self.texture, z_index=self.z_index)
+            point = ASCIIPoint2D(self, x=int(position.x), y=int(position.y), texture=self.texture, z_index=self.z_index, force_sort=self.force_sort)
             self.points.append(point)
     
     def queue_free(self) -> None:
-        """Queues all points for deletion before calling its super().queue_free()
+        """Queues all points for deletion before calling super().queue_free()
         """
         for point in self.points:
             point.queue_free()
