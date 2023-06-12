6b0eb7f5e6047f4255a4a3ad8d9e2e60c2b89bd1
Floating-Int <knutolaihelgesen@gmail.com>
2023-06-12 19:02:41 +0200

    Changed default naming to exlude Ascii in the names of the classes

--- displaylib/__init__.py
+++ displaylib/__init__.py
@@ -1,6 +1,10 @@
 """# DisplayLib
 ----------------
 
+An object-oriented framework for displaying ASCII graphics and creating an infinite world, aimed at simplifying the process
+
+----------------
+
 `Requires Python version >= 3.10`
 
 ----------------
@@ -24,28 +28,32 @@ __version__ = "0.0.8"
 __author__ = "Floating-Int"
 __all__ = [ # default mode is used when using the star notation
     # math
-    "lerp",
-    "sign",
-    "Vec2",
-    "Vec2i",
+    "lerp",              # (function)
+    "sign",              # (function)
+    "Vec2",              # (data structure)
+    "Vec2i",             # (data structure)
     # utility
-    "pull",
-    "graphme",
-    # standard
-    "Node",
-    "Node2D",
+    "pull",              # (decorator)
+    "grapheme",          # (module)
     # core ascii
-    "AsciiNode2D",
-    "AsciiEngine",
-    "AsciiCamera",
-    "AsciiSurface",
-    "AsciiScreen",
-    "AsciiImage",
-    "AsciiLabel",
-    "AsciiLine",
-    "AsciiSprite",
-    # networking (module)
-    "networking"
+    "Node",              # (class)
+    "Node2D",            # (class)
+    "Engine",            # (class)
+    "Camera",            # (class)
+    "Screen",            # (class)
+    "Frame",             # (class)
+    "Animation",         # (class)
+    "EmptyAnimation",    # (class)
+    "AnimationPlayer",   # (class)
+    "AudioStreamPlayer", # (class)
+    "Clock",             # (class)
+    # prefabricated
+    "Image",             # (class)
+    "Label",             # (class)
+    "Line",              # (class)
+    "Sprite",            # (class)
+    # networking
+    "networking"         # (module)
 ]
 
 # math
@@ -53,27 +61,26 @@ from .math import lerp, sign, Vec2, Vec2i
 # utility
 from .util import pull
 # standard
-from .template import Node, Node2D
 from .ascii import (
     # utility
-    grapheme as graphme, # (module)
-    # ascii nodes
-    Node2D as AsciiNode2D,
-    Engine as AsciiEngine,
-    Camera as AsciiCamera,
-    Surface as AsciiSurface,
-    Screen as AsciiScreen,
-    Image as AsciiImage,
-    Frame as Frame,
-    Animation as Animation,
-    EmptyAnimation as EmptyAnimation,
-    AnimationPlayer as AnimationPlayer,
-    AudioStreamPlayer as AudioStreamPlayer,
-    Clock as Clock,
+    grapheme, # (module)
+    # core ascii
+    Node,
+    Node2D,
+    Engine,
+    Camera,
+    Screen,
+    Image,
+    Frame,
+    Animation,
+    EmptyAnimation,
+    AnimationPlayer,
+    AudioStreamPlayer,
+    Clock,
     # prefabricated
-    Label as AsciiLabel,
-    Line as AsciiLine,
-    Sprite as AsciiSprite,
+    Label,
+    Line,
+    Sprite,
     # networking
     networking # (module)
 )
