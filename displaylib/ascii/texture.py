f26edf8e4813e44465c0b1595545565b8d64225e
Floating-Int <knutolaihelgesen@gmail.com>
2023-06-19 13:23:03 +0200

    Methods for making the texture unique

--- displaylib/ascii/texture.py
+++ displaylib/ascii/texture.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import copy
 from typing import TypeVar, ClassVar
 
+from ..math import Vec2
 from ..template import Transform2D
 
 Self = TypeVar("Self")
@@ -16,14 +18,21 @@ class Texture: # Component (mixin class)
     _instances: ClassVar[list[Texture]] = [] # references to nodes with Texture component
     _request_z_index_sort: ClassVar[bool] = False # requests Engine to sort
     texture: list[list[str]]
-    visible: bool
+    offset: Vec2
+    centered: bool
 
-    def __new__(cls: type[Self], *args, texture: list[list[str]] = [], z_index: int = 0, force_sort: bool = True, **kwargs) -> Self:
+    def __new__(cls: type[Self], *args, texture: list[list[str]] = [], offset: Vec2 = Vec2(0, 0), centered: bool = False, z_index: int = 0, force_sort: bool = True, **kwargs) -> Self:
         instance = super().__new__(cls, *args, force_sort=force_sort, **kwargs) # `force_sort` is passed to Node eventually
         if not isinstance(instance, Transform2D):
-            raise TypeError(f"in class '{instance.__class__.__qualname__}': mixin class '{__class__.__qualname__}' requires to be used in combination with a node class deriving from 'Transform2D'")
+            raise TypeError(f"class '{__class__.__qualname__}' is required to derive from 'Transform2D' as it derives from 'Texture'")
+        # set if not defined in class
         if not getattr(instance, "texture", False):
             setattr(instance, "texture", list())
+        # set anyway
+        setattr(instance, "offset", Vec2())
+        setattr(getattr(instance, "offset"), "x", offset.x)
+        setattr(getattr(instance, "offset"), "y", offset.y)
+        setattr(instance, "centered", centered)
         setattr(instance, "_z_index", z_index)
         if force_sort:
             Texture._request_z_index_sort = True
@@ -32,17 +41,58 @@ class Texture: # Component (mixin class)
     
     @property
     def z_index(self) -> int:
+        """Returns the z_index of this node
+
+        Returns:
+            int: z_index
+        """
         return self._z_index
 
     @z_index.setter
     def z_index(self, value: int) -> None:
+        """Sets the `.z_index` and requests the engine to sort textured nodes based on this
+
+        Args:
+            value (int): new z_index
+        """
         if self._z_index != value: # if changed
             self._z_index = value
             Texture._request_z_index_sort = True
+    
+    def get_texture_global_position(self) -> Vec2:
+        """Calculates where the texture starts, after taking `.offset` into consideration (world space)
+
+        Returns:
+            Vec2: global position of the texture
+        """
+        global_position = self.position + self.offset
+        if self.centered:
+            global_position.x += len(max(self.texture, key=len))
+            global_position.y += len(self.texture)
+        parent = self.parent
+        while parent is not None and isinstance(parent, Transform2D):
+            global_position = parent.position + global_position.rotated(parent.rotation)
+            parent = parent.parent
+        return global_position
+
+    def make_unique(self) -> None:
+        """Makes a deepcopy of `.texture`, which is then set as the new texture
+        """
+        self.texture = copy.deepcopy(self.texture) # from class var to instance var (if class var defined)
+    
+    def as_unique(self: Self) -> Self:
+        """Makes a deepcopy of `.texture`, which is then set as the new texture,
+        along returning itself
+
+        Returns:
+            Self: itself after texture is made unique
+        """
+        getattr(self, "make_unique")()
+        return self
 
     def queue_free(self) -> None:
-        """Decrements this node's reference by removing it from `Texture._instances`.
-        Then queues the node to be deleted by the engine
+        """Decrements the reference of the node by removing it from `Texture._instances`
+        and then adds it to the deletion queue of the engine
         """
         if self in Texture._instances:
             Texture._instances.remove(self)
