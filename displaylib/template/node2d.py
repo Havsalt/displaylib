f4ef126dc26a55e0ffb8aa0d827f74a08596f60c
Floating-Int <knutolaihelgesen@gmail.com>
2023-06-26 13:31:35 +0200

    Now sets the .position in __init__

--- displaylib/template/node2d.py
+++ displaylib/template/node2d.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from ..math import Vec2
 from .node import Node
 from .transform import Transform2D
 
@@ -12,3 +13,4 @@ class Node2D(Transform2D, Node):
     """
     def __init__(self, parent: Node | None = None, *, x: int | float = 0, y: int | float = 0, force_sort: bool = True) -> None:
         super().__init__(parent, force_sort=force_sort) # parameters `x` and `y` is set in `Transform`
+        self.position = Vec2(x, y)
