8f648f09f513976bd40fbbd8ead30496471c50e9
Floating-Int <knutolaihelgesen@gmail.com>
2023-06-30 14:36:25 +0200

    Small fixes

--- displaylib/ascii/prefabs/sprite.py
+++ displaylib/ascii/prefabs/sprite.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 import io
 import os
 import functools
-from typing import TypeVar
+from typing import TYPE_CHECKING, TypeVar
 
 from ...math import Vec2, Vec2i
-from ...template import Node
 from ..node import AsciiNode2D
 from ..texture import Texture
+
+if TYPE_CHECKING:
+    from ...template import Node
 
 Self = TypeVar("Self")
 
@@ -32,7 +34,7 @@ class AsciiSprite(Texture, AsciiNode2D):
     Known Issues:
         - `If a file's content is changed after a texture has been loaded from that file, the change won't be reflected on next load due to the use of @functools.cache`
     """
-    texture: list[list[str]] # NOTE: class texture is shared lists across instances
+    texture: list[list[str]] # NOTE: class texture is shared lists across instances unless .make_unique() or .as_unique()
 
     @classmethod
     def load(cls, file_path: str, /) -> AsciiSprite:
@@ -43,22 +45,15 @@ class AsciiSprite(Texture, AsciiNode2D):
 
         Raises:
             TypeError: file_path was not a string
-            ValueError: file_path did not end with the correct extension (.txt)
 
         Returns:
             AsciiSprite: sprite give its texture from file content
         """
         if not isinstance(file_path, str):
             TypeError(f"argument 'file_path' is required to be of type 'str'. '{type(file_path).__name__}' found")
-        
         fpath = os.path.normpath(file_path)
-        
-        if not fpath.endswith(".txt"):
-            raise ValueError("argument 'file_path' needs to end with the current extension of '" + cls.extension + "'")
-        
-        texture = load_texture(file_path)
-        sprite = AsciiSprite(texture=texture)
-        return sprite
+        texture = load_texture(fpath)
+        return AsciiSprite(texture=texture)
 
     def __init__(self, parent: Node | None = None, *, x: int | float = 0, y: int | float = 0, texture: list[list[str]] = [], offset: Vec2 = Vec2(0, 0), centered: bool = False, z_index: int = 0, force_sort: bool = True) -> None: # `z_index` pulled in `Texture`
         super().__init__(parent, x=x, y=y, force_sort=force_sort)
@@ -71,7 +66,7 @@ class AsciiSprite(Texture, AsciiNode2D):
         """Returns the width and height of `.texture` as a vector
 
         Returns:
-            Vec2: size of the content
+            Vec2i: size of the content
         """
         longest = len(max(self.texture, key=len))
         lines = len(self.texture)
