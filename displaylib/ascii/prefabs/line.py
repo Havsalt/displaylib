74c6f1c2587b68b5ddeecb65c51f524dfb4d950c
Floating-Int <knutolaihelgesen@gmail.com>
2023-07-01 16:51:39 +0200

    Integrated Color component

--- displaylib/ascii/prefabs/line.py
+++ displaylib/ascii/prefabs/line.py
@@ -6,20 +6,24 @@ from ...math import Vec2
 from ...util import pull
 from ..node import AsciiNode2D
 from ..texture import Texture
+from ..colored import Color
+from ..color import WHITE, _Color
 
 if TYPE_CHECKING:
     from ...template import Node
 
 
-class AsciiPoint2D(Texture, AsciiNode2D):
+class AsciiPoint2D(Color, Texture, AsciiNode2D):
     """Thin wrapper around `AsciiNode2D` capable of displaying a single point
     
     Components:
-        `Texture`: allows the node to be shown
+        - `Texture`: allows the node to be shown
+        - `Color`: applies color to the texture
     """
-    def __init__(self, parent: Node | None = None, *, x: float = 0, y: float = 0, texture: list[list[str]] = [["#"]], z_index: int = 0, force_sort: bool = True) -> None:
+    def __init__(self, parent: Node | None = None, *, x: float = 0, y: float = 0, texture: list[list[str]] = [["#"]], color: _Color = WHITE, z_index: int = 0, force_sort: bool = True) -> None:
         super().__init__(parent, x=x, y=y, force_sort=force_sort)
         self.texture = texture
+        self.color = color
         self.z_index = z_index
 
 
@@ -43,7 +47,7 @@ class AsciiLine(AsciiNode2D):
         self.points: list[AsciiPoint2D] = []
         self._update(0) # simulate initial frame locally
 
-    def _update(self, delta: float) -> None:
+    def _update(self, _delta: float) -> None:
         # clear points
         for point in self.points:
             point.queue_free()
