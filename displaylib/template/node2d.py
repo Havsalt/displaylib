5356471c367676874aeedecf2f51201d83acb01d
Floating-Int <knutolaihelgesen@gmail.com>
2023-08-03 17:09:31 +0200

    Removed __init__ body

--- displaylib/template/node2d.py
+++ displaylib/template/node2d.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
-from ..math import Vec2
+from typing import TYPE_CHECKING
+
 from .node import Node
 from .transform import Transform2D
+
+if TYPE_CHECKING:
+    from .type_hints import AnyNode
 
 
 class Node2D(Transform2D, Node):
@@ -11,14 +15,12 @@ class Node2D(Transform2D, Node):
     Components:
         `Transform2D`: provides position and rotation attributes
     """
-    def __init__(self, parent: Node | None = None, *, x: float = 0, y: float = 0, force_sort: bool = True) -> None:
+    def __init__(self, parent: AnyNode | None = None, *, x: float = 0, y: float = 0, force_sort: bool = True) -> None:
         """Initializes the node in 2D space
 
         Args:
-            parent (Node | None, optional): parent node. Defaults to None.
+            parent (AnyNode | None, optional): parent node. Defaults to None.
             x (float, optional): local x position. Defaults to 0.
             y (float, optional): local y position. Defaults to 0.
             force_sort (bool, optional): whether to sort based on 'z_index' and 'process_priority'. Defaults to True.
         """
-        super().__init__(parent, force_sort=force_sort) # parameters `x` and `y` is set in `Transform`
-        self.position = Vec2(x, y)
