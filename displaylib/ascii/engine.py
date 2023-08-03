35c6e6b86a5d1b050bd22e3f4975de797e9bd85d
Floating-Int <knutolaihelgesen@gmail.com>
2023-08-03 17:19:43 +0200

    Moved code to __new__

--- displaylib/ascii/engine.py
+++ displaylib/ascii/engine.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import os
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
 from ..math import Vec2i
 from ..template import Node, Engine
+from ..template.type_hints import MroNext, EngineType
 from .clock import Clock
 from .screen import AsciiScreen
 from .camera import AsciiCamera
@@ -24,36 +25,41 @@ class AsciiEngine(Engine):
         - `_update(self, delta: float) -> None`
         - `_on_screen_resize(self, size: Vec2i) -> None`
     """
+    auto_resize_screen: bool
+    screen_margin: Vec2i
+    screen: AsciiScreen
 
-    def __new__(cls: type[AsciiEngine], *args, **kwargs) -> AsciiEngine:
+    def __new__(cls: type[EngineType], *, tps: int = 16, width: int = 16, height: int = 8, initial_clear: bool = False, auto_resize_screen: bool = False, screen_margin: Vec2i = Vec2i(1, 1), **config) -> EngineType:
         """Sets `Node.root` when an Engine instance is created. Initializes default `AsciiCamera`
 
         Args:
-            cls (type[AsciiEngine]): engine object to be root
+            cls (type[EngineType]): engine object to be root
 
         Returns:
-            AsciiEngine: the engine to be used in the program
+            EngineType: the engine to be used in the program
         """
-        instance = super().__new__(cls)
+        mro_next = cast(MroNext[AsciiEngine], super())
+        instance = mro_next.__new__(cls, tps=tps, **config)
+        instance.tps = tps
+        instance.auto_resize_screen = auto_resize_screen
+        instance.screen_margin = screen_margin
+        instance.screen = AsciiScreen(width=width, height=height)
+        
+        if auto_resize_screen:
+            terminal_size = os.get_terminal_size()
+            if terminal_size.columns != instance.screen.width or terminal_size.lines != instance.screen.height:
+                instance.screen.width = int(terminal_size.columns - instance.screen_margin.x)
+                instance.screen.height = int(terminal_size.lines - instance.screen_margin.y)
+                if not initial_clear: # only clear once, so wait a bit to do it anyways
+                    os.system("cls")
+
+        if initial_clear:
+            os.system("cls")
+
         if not hasattr(AsciiCamera, "current"):
             camera = AsciiCamera()
-            setattr(AsciiCamera, "current", camera) # initialize default camera
-        # # enable mouse events
-        # if getattr(instance, "mouse", False) or instance.__annotations__.get("mouse", False):
-        #     # setup a handler for updating the mouse
-        #     def _update_mouse(self) -> None:
-        #         mouse_events = []
-        #         mouse_position = MouseMotionEvent(position=get_mouse_position())
-        #         mouse_events.append(mouse_position)
-        #         for event in mouse_events:
-        #             self._on_mouse_event(event)
-        #         for node in tuple(Node.nodes.values()):
-        #             if isinstance(node, Ascii):
-        #                 for mouse_event in mouse_events:
-        #                     node._on_mouse_event(mouse_event)
-        #     setattr(instance, "_update_mouse", types.MethodType(_update_mouse, instance))
-        #     instance.per_frame_tasks.append(getattr(instance, "_update_mouse"))
-        return instance
+            AsciiCamera.current = camera # initialize default camera
+        return cast(EngineType, instance)
 
     def __init__(self, *, tps: int = 16, width: int = 16, height: int = 8, initial_clear: bool = False, auto_resize_screen: bool = False, screen_margin: Vec2i = Vec2i(1, 1), **config) -> None:
         """Initializes and starts the engine (only 1 instance should exist)
@@ -66,20 +72,7 @@ class AsciiEngine(Engine):
             auto_resize_screen (bool, optional): whether to automatically resize the screen to fit. Defaults to False.
             screen_margin (Vec2i, optional): subtracted from os terminal size. Defaults to Vec2i(1, 1).
         """
-        self.tps = tps
-        self.auto_resize_screen = auto_resize_screen
-        self.screen_margin = screen_margin # TODO: add setter/getter to force screen update
-        self.screen = AsciiScreen(width=width, height=height)
-        if auto_resize_screen:
-            terminal_size = os.get_terminal_size()
-            if terminal_size.columns != self.screen.width or terminal_size.lines != self.screen.height:
-                self.screen.width = int(terminal_size.columns - self.screen_margin.x)
-                self.screen.height = int(terminal_size.lines - self.screen_margin.y)
-                if not initial_clear: # only clear once, so wait a bit to do it anyways
-                    os.system("cls")
-        if initial_clear:
-            os.system("cls")
-        super(__class__, self).__init__(**config)
+        super().__init__(tps=tps)
     
     def _on_screen_resize(self, size: Vec2i) -> None:
         """Override for custom functionality
@@ -115,7 +108,7 @@ class AsciiEngine(Engine):
                     os.system("cls")
                 
             for task in self.per_frame_tasks:
-                task()
+                task() # type: ignore
             
             self._update(clock.delta_time)
             for node in tuple(Node.nodes.values()): # tuple, because removing a ref in lets say an list will free the node during iteration
@@ -123,7 +116,8 @@ class AsciiEngine(Engine):
 
             if Node._request_process_priority_sort: # only sort once per frame if needed
                 for uid in Node._queued_nodes:
-                    del Node.nodes[uid]
+                    if uid in Node.nodes: # FIXME: temp
+                        del Node.nodes[uid]
                 Node._queued_nodes.clear()
                 Node.nodes = {uid: node for uid, node in sorted(Node.nodes.items(), key=self.sort_function_for_process_priority)}
             if Texture._request_z_index_sort:
@@ -135,7 +129,7 @@ class AsciiEngine(Engine):
             self.screen.show()
             clock.tick()
         
-        # v exit protocol v
-        self._on_exit()
+        self.screen.clear()
         self.screen.render(Texture._instances)
         self.screen.show()
+        # _on_exit() is called automatically after this
