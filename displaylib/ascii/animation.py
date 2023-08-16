d173b13c743871503f235607dd339305aa5999dd
Floating-Int <knutolaihelgesen@gmail.com>
2023-08-16 20:22:06 +0200

    Manipulating attributes instead of using a generator

--- displaylib/ascii/animation.py
+++ displaylib/ascii/animation.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 
 import os
-from typing import TYPE_CHECKING, Generator, cast
+from typing import TYPE_CHECKING, Protocol, cast
 
 from ..template import Node
 from ..template.type_hints import MroNext, NodeType
 from .texture import Texture, load_texture
 
 if TYPE_CHECKING:
-    from ..template.type_hints import AnyNode
+    from ..template.type_hints import NodeMixin
+    from .type_hints import TextureMixin
+    class AnyTextureNode(TextureMixin, NodeMixin, Protocol): ...
 
 
 class AnimationFrame:
@@ -37,6 +39,7 @@ class Animation:
     Frames are loaded from files or partially from cache
     """
     __slots__ = ("frames")
+    frames: list[AnimationFrame]
 
     def __init__(self, folder_path: str, /, *, reverse: bool = False, fill: bool = True, fliph: bool = False, flipv: bool = False) -> None:
         """Loads and animation with frames from either files or partially from cache
@@ -75,52 +78,26 @@ class AnimationPlayer(Node):
         - `If a file's content is changed after a texture has been loaded from that file, the change won't be reflected on next load due to the use of @functools.cache`
     """
     animations: dict[str, Animation]
-    current_animation: str
-    is_playing: bool
-    _current_frames: Generator[AnimationFrame, None, None] | None
-    _next: AnimationFrame | None
-    _has_updated: bool
-    _accumulated_time: float
+    frame: int = 0
+    current_animation: str = ""
+    is_playing: bool = False
+    reverse_playback: bool = False
+    _has_updated: bool = True # indicates if the first frame (per animation) has been displayed
+    _accumulated_time: float = 0
 
     def __new__(cls: type[NodeType], *args, **animations) -> NodeType:
         mro_next = cast(MroNext[AnimationPlayer], super())
         instance = mro_next.__new__(cls, *args, force_sort=False) # because this cannot be passed as an argument, force_sort is set to False
-        instance.animations = dict(animations)
-        instance.current_animation = ""
-        instance.is_playing = False
-        instance._current_frames = None
-        instance._next = None
-        instance._has_updated = False # indicates if the first frame (per animation) has been displayed
-        instance._accumulated_time = 0.0
+        instance.animations = dict(animations) # make unique
         return cast(NodeType, instance)
 
-    def __init__(self, parent: AnyNode | None = None, **animations: Animation) -> None:
+    def __init__(self, parent: AnyTextureNode, **animations: Animation) -> None:
         """Initializes the animation player
 
         Args:
             parent (ValidTextureNode | None, optional): parent node with Texture and Transform2D components. Defaults to None.
             **animations (Animation): animations are stored as {str: Animation, ...} pairs
-
-        Raises:
-            TypeError: 'parent' missing component Texture
         """
-
-    def __next__(self) -> AnimationFrame | None:
-        """Returns the next frame from the current frames (a generator)
-
-        Returns:
-            AnimationFrame: the next frame
-        """
-        try:
-            if self._current_frames is None:
-                return None
-            self._next = next(self._current_frames) # next of generator
-            return self._next
-        except StopIteration:
-            self.is_playing = False
-            self._current_frames = None
-            self._next = None
-            return None
 
     @property
     def active_animation(self) -> Animation | None:
@@ -129,96 +106,121 @@ class AnimationPlayer(Node):
         Returns:
             Animation | None: active animaion if any active, else None
         """
-        return self.animations.get(self.current_animation, None)
+        if not self.current_animation: # empty string, which is default
+            return None
+        return self.animations[self.current_animation]
     
     @active_animation.setter
-    def active_animation(self, animation: str) -> None:
+    def active_animation(self, animation_name: str) -> None:
         """Sets the next frames based on animation name
 
         Args:
-            animation (str): Animation object to be used
+            animation_name (str): Animation object to be used
+
+        Raises:
+            ValueError: invalid animation name
         """
-        self.current_animation = animation
-        # make generator
-        self._current_frames = (frame for frame in self.animations[animation].frames)
-        try:
-            self._next = next(self._current_frames)
-        except StopIteration:
+        if animation_name not in self.animations:
+            raise ValueError(f"animation not found: '{animation_name}'")
+
+        if self.parent is None or not isinstance(self.parent, Texture):
+            raise TypeError("parent requires component 'Texture'")
+
+        animation = self.animations[animation_name]
+        self.current_animation = animation_name
+        self.frame = 0
+
+        if not animation.frames:
+            self._has_updated = True
             self.is_playing = False
-            self._current_frames = None
-            self._next = None
+            return
+        
+        self.is_playing = True
+        self.parent.texture = animation.frames[0].texture
+        self._has_updated = False
     
-    def get(self, name: str) -> Animation | None:
+    def get(self, animation_name: str) -> Animation | None:
         """Returns a stored animation given its name
 
         Args:
-            name (str): name of the animation
+            animation_name (str): name of the animation
 
         Returns:
             Animation | None: animation object or None if not found
         """
-        return self.animations.get(name, None)
+        return self.animations.get(animation_name, None)
     
-    def add(self, name: str, animation: Animation) -> None:
+    def add(self, animation_name: str, animation: Animation) -> None:
         """Adds a new animation and binds it to a name
 
         Args:
-            name (str): name to access the animation later
+            animation_name (str): name to access the animation later
             animation (Animation): animation object to store
         """
-        self.animations[name] = animation
+        self.animations[animation_name] = animation
     
-    def remove(self, name: str) -> None:
+    def remove(self, animation_name: str) -> None:
         """Removes an animation given the name of the animation
 
         Args:
-            name (str): name of the animation to delete
+            animation_name (str): name of the animation to delete
         """
-        del self.animations[name]
+        del self.animations[animation_name]
     
-    def play(self, animation: str) -> None:
+    def play(self, animation_name: str) -> None:
         """Plays an animation given the name of the animation
 
         Args:
             animation (str): the name of the animation to play
+        
+        Raises:
+            ValueError: invalid animation name
         """
-        self.is_playing = True
-        self.current_animation = animation
-        # make generator
-        self._current_frames = (frame for frame in self.animations[animation].frames)
-        try:
-            self._next = next(self._current_frames)
-        except StopIteration:
-            self.is_playing = False
-            self._current_frames = None
-            self._next = None
-        if not isinstance(self.parent, Texture):
+        if animation_name not in self.animations:
+            raise ValueError(f"animation not found: '{animation_name}'")
+        
+        if self.parent is None or not isinstance(self.parent, Texture):
             raise TypeError("parent requires component 'Texture'")
-        if self._next is not None and self.parent is not None:
-            self.parent.texture = self._next.texture
-            self._has_updated = False
+        
+        self.reverse_playback = False
+        animation = self.animations[animation_name]
+        self.current_animation = animation_name
+        self.frame = 0
+
+        if not animation.frames:
+            self._has_updated = True
+            self.is_playing = False
+            return
+        
+        self.is_playing = True
+        self.parent.texture = animation.frames[0].texture
+        self._has_updated = False
     
-    def play_backwards(self, animation: str) -> None:
+    def play_backwards(self, animation_name: str) -> None:
         """Plays an animation backwards given the name of the animation
 
         Args:
-            animation (str): the name of the animation to play backwards
+            animation_name (str): the name of the animation to play backwards
         """
-        self.is_playing = True
-        self.current_animation = animation
-        # reverse order frames generator
-        self._current_frames = (frame for frame in reversed(self.animations[animation].frames))
-        try:
-            self._next = next(self._current_frames)
-        except StopIteration:
-            self.is_playing = False
-            self._current_frames = None
-            self._next = None
-        if not isinstance(self.parent, Texture):
+        if animation_name not in self.animations:
+            raise ValueError(f"animation not found: '{animation_name}'")
+        
+        if self.parent is None or not isinstance(self.parent, Texture):
             raise TypeError("parent requires component 'Texture'")
-        if self._next is not None and self.parent is not None:
-            self.parent.texture = self._next.texture
-            self._has_updated = False
+        
+        self.reverse_playback = True
+        animation = self.animations[animation_name]
+        self.current_animation = animation_name
+        self.frame = len(animation.frames) -1
+
+        if not animation.frames:
+            self._has_updated = True
+            self.is_playing = False
+            return
+        
+        self.is_playing = True
+        self.parent.texture = animation.frames[-1].texture
+        self._has_updated = False
     
     def advance(self) -> bool:
         """Advances 1 frame
@@ -230,35 +232,63 @@ class AnimationPlayer(Node):
         Returns:
             bool: whether it continues the animation (was NOT stopped)
         """
-        if self._current_frames == None:
-            return False
-        try:
-            self._next = next(self._current_frames)
-        except StopIteration:
-            self.is_playing = False
-            self._current_frames = None
-            self._next = None
-        if not isinstance(self.parent, Texture):
-            raise TypeError("parent requires component 'Texture'")
-        if self._next is not None and self.parent is not None:
-            self.parent.texture = self._next.texture
-            self._has_updated = False
-        return self._next != None # returns true if not stopped
+        if self.current_animation != "" and not self.current_animation in self.animations:
+            raise ValueError(f"current animation name is invalid: '{self.current_animation}'")
+        
+        if self.parent is None or not isinstance(self.parent, Texture):
+            raise TypeError(f"parent requires component 'Texture', not '{type(self.parent).__qualname__}'")
+
+        animation = self.animations[self.current_animation]
+        
+        # check playback mode and its future index
+        if self.reverse_playback:
+            if (self.frame -1) < 0:
+                if animation.frames:
+                    first_animation_frame = animation.frames[0]
+                    self.parent.texture = first_animation_frame.texture
+        else:
+            if (self.frame +1) >= len(animation.frames):
+                if animation.frames:
+                    last_animation_frame = animation.frames[-1]
+                    self.parent.texture = last_animation_frame.texture
+                self.is_playing = False
+                return False
+        
+        if self.reverse_playback:
+            self.frame -= 1
+        else:
+            self.frame += 1
+        animation_frame = animation.frames[self.frame]
+        self.parent.texture = animation_frame.texture
+        return True # returns true if not stopped
 
     def stop(self) -> None:
         """Stops the animation from playing
         """
         self.is_playing = False
+        self._has_updated = False
     
     def _update(self, _delta: float) -> None:
         """Handles updating the parent's texture if an animation is playing
         """
+        if self.current_animation != "" and not self.current_animation in self.animations:
+            raise ValueError(f"current animation name is invalid: '{self.current_animation}'")
+            
+        if self.parent is None or not isinstance(self.parent, Texture):
+            raise TypeError("parent requires component 'Texture'")
+
         if self.is_playing and self._has_updated:
-            frame = next(self)
-            if not isinstance(self.parent, Texture):
-                raise TypeError("parent requires component 'Texture'")
-            if frame is None or self.parent is None:
-                return
-            self.parent.texture = frame.texture
+            animation = self.active_animation
+            if animation is not None:
+                # check playback mode and future index
+                if self.reverse_playback and (self.frame -1) >= 0:
+                    self.frame -= 1
+                    self.parent.texture = animation.frames[self.frame].texture
+                elif not self.reverse_playback and (self.frame +1) < len(animation.frames):
+                    self.frame += 1
+                    self.parent.texture = animation.frames[self.frame].texture
+                else: # both is out of bounds, therefore, stop
+                    self.is_playing = False
+
         elif not self._has_updated:
             self._has_updated = True
