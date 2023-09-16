724486ff6d20e216407845c775f6108f7edaa70b
Floating-Int <knutolaihelgesen@gmail.com>
2023-09-16 10:42:21 +0200

    Moved implementation to __new__

--- displaylib/template/networking/server.py
+++ displaylib/template/networking/server.py
@@ -2,10 +2,44 @@ from __future__ import annotations
 
 import selectors
 import socket
+from typing import TYPE_CHECKING, Protocol, cast
+
+from ..type_hints import MroNext, EngineType, EngineMixin
+
+if TYPE_CHECKING:
+    from type_hints import FunctionType
+
+class ServerMixin(Protocol):
+    @property
+    def _address(self) -> tuple[str | int, int]: ...
+    @_address.setter
+    def _address(self, value: tuple[str | int, int]) -> None: ...
+    @property
+    def _selector(self) -> selectors.DefaultSelector: ...
+    @_selector.setter
+    def _selector(self, value: selectors.DefaultSelector) -> None: ...
+    @property
+    def _socket(self) -> socket.socket: ...
+    @_socket.setter
+    def _socket(self, value: socket.socket) -> None: ...
+    @property
+    def _buffer(self) -> bytes: ...
+    @_buffer.setter
+    def _buffer(self, value: bytes) -> None: ...
+    @property
+    def _update_socket(self) -> FunctionType: ...
+    def _on_connection_refused(self, error: Exception) -> None: ...
+    def _on_client_connected(self, connection: socket.socket, host: str, port: int) -> None: ...
+    def _on_client_disconnected(self, connection: socket.socket, error: Exception) -> None: ...
+    def _on_request_received(self, sender: socket.socket, request: bytes) -> None: ...
+    def _on_system_request(self, request: dict[str, str]) -> None: ...
+    def _on_custom_request(self, request: dict[str, str]) -> None: ...
+
+class ValidServer(ServerMixin, EngineMixin, Protocol): ...
 
 
 class Server:
-    """`Server` base class
+    """`Server` mixin class
 
     Hooks:
         - `_on_connection_refused(self, error: Exception) -> None`
@@ -18,30 +52,39 @@ class Server:
     buffer_size: int = 4096
     timeout: float = 0
     encoding: str = "utf-8"
+    _socket: socket.socket
+    _address: tuple[str, int]
+    _selector: selectors.DefaultSelector
 
-    def __init__(self, *, host: str = "localhost", port: int = 8080, backlog: int = 4, **config) -> None:
-        """Initializes `Server` functionality on the `Engine`
+    def __new__(cls: type[EngineType], *, host: str = "localhost", port: int = 8080, backlog: int = 4, **config) -> EngineType:
+        """Adds `Server` functionality on the `Engine`
 
-        Args:
+        Added Args:
             host (str, optional): host name. Defaults to "localhost".
             port (int, optional): port number. Defaults to 8080.
             backlog (int, optional): number of connections allowed to connect at once. Defaults to 4.
         """
-        self._address = (host, port)
-        self._selector = selectors.DefaultSelector()
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._selector.register(self._socket, selectors.EVENT_READ)
-        # self._buffer = bytes()
+        mro_next = cast(MroNext[ValidServer], super())
+        instance = mro_next.__new__(cls, **config)
+        # class value -> override -> default
+        host = getattr(instance, "host", host)
+        # class value -> override -> default
+        port = getattr(instance, "port", port)
+        instance._address = (host, port)
+        instance._selector = selectors.DefaultSelector()
+        instance._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        instance._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        instance._selector.register(instance._socket, selectors.EVENT_READ)
+        instance._buffer = bytes()
         try:
-            self._socket.bind(self._address)
-            self._socket.listen(backlog)
+            instance._socket.bind(instance._address)
+            instance._socket.listen(backlog)
         except ConnectionRefusedError as error:
-            self._on_connection_refused(error)
-            return # does not start the main loop if failed to connect
-        self._socket.setblocking(False)
-        self.per_frame_tasks.append(self._update_socket) # Engine task
-        super(__class__, self).__init__(**config)
+            instance._on_connection_refused(error)
+            return cast(EngineType, instance) # does not start the main loop if failed to connect
+        instance._socket.setblocking(False)
+        instance.per_frame_tasks.append(instance._update_socket)
+        return cast(EngineType, instance)
 
     def _on_connection_refused(self, error: Exception) -> None:
         """Override for custom functionality
@@ -97,9 +140,9 @@ class Server:
     def _update_socket(self) -> None:
         """Updates the socket's I/O and calls `_on_request_received` with the bytes received as argument
         """
-        # -- recieve request
+        # recieve request
         for key, mask in self._selector.select(timeout=self.timeout):
-            connection = key.fileobj
+            connection = cast(socket.socket, key.fileobj)
             if mask & selectors.EVENT_READ:
                 try:
                     request: bytes = connection.recv(self.buffer_size)
@@ -109,7 +152,7 @@ class Server:
                     continue
                 if request:
                     self._on_request_received(connection, request)
-        # -- accept connection if one is incoming
+        # accept connection if one is incoming
         try:
             connection, (host, port) = self._socket.accept()
             self._selector.register(connection, selectors.EVENT_READ)
