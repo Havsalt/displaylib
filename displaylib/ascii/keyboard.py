6ce749a1e9c6720f5d819d0d682e3e8f48062464
Floating-Int <knutolaihelgesen@gmail.com>
2023-09-27 18:47:58 +0200

    Added windows fallbacks if keyboard library is not installed

--- displaylib/ascii/keyboard.py
+++ displaylib/ascii/keyboard.py
@@ -1,19 +1,78 @@
 from __future__ import annotations as _annotations
 
+from typing import TypeAlias as _TypeAlias
 
-def is_pressed(hotkey: _Key | _ScanCodeList) -> bool:
+_Key: _TypeAlias = int | str
+
+
+def is_pressed(key: _Key, /) -> bool:
     # TODO: make implementation
     return False
 
+
 try:
     import keyboard as _keyboard
-    from typing import TypeAlias as _TypeAlias
 
-    _Key: _TypeAlias = int | str
-    _ScanCodeList: _TypeAlias = list[int] | tuple[int, ...]
+    def is_pressed(key: _Key, /) -> bool:
+        return _keyboard.is_pressed(key)
 
-    def is_pressed(hotkey: _Key | _ScanCodeList) -> bool:
-        return _keyboard.is_pressed(hotkey)
 
 except ModuleNotFoundError:
-    pass
+    import os as _os
+    
+    if _os.name == "nt":  # Windows
+        import msvcrt as _msvcrt
+
+        _key_to_scancode: dict[str, int] = {
+            # defined at runtime to match keys on physical keyboard better
+            "0": ord("0"),
+            "1": ord("1"),
+            "2": ord("2"),
+            "3": ord("3"),
+            "4": ord("4"),
+            "5": ord("5"),
+            "6": ord("6"),
+            "7": ord("7"),
+            "8": ord("8"),
+            "9": ord("9"),
+            "space": ord(" "),
+            "enter": ord("\r"),
+            "del": ord("\b"),
+            "delete": ord("\b"),
+            "tab": ord("\t"),
+            "bar": ord("|"),
+            "esc": ord("\x1b"),
+            "escape": ord("\x1b")
+        }
+
+        def is_pressed(key: _Key, /) -> bool:
+            if isinstance(key, str):
+                key = key.lower()
+                if key in _key_to_scancode:
+                    scancode = _key_to_scancode[key]
+                elif len(key) == 1:
+                    scancode = ord(key)
+                else:
+                    raise ValueError(f"key '{key}' of type 'str' was not recognized")
+            elif isinstance(key, int):
+                scancode = key
+            else:
+                raise ValueError("parameter 'key' has to be of type 'int' or 'str'")
+            all_pressed = []
+            while _msvcrt.kbhit():
+                pressed = _msvcrt.getch()
+                all_pressed.append(pressed)
+                if ord(pressed) == scancode or (ord(pressed) + 32 == scancode and 65 <= ord(pressed) <= 90):
+                    all_pressed.pop()
+                    for prev_pressed in all_pressed:
+                        _msvcrt.ungetch(prev_pressed)
+                    return True
+            for prev_pressed in all_pressed:
+                _msvcrt.ungetch(prev_pressed)
+            return False
+    
+    
+    # elif _os.name == "posix":  # Linux and macOS
+    #     pass
+    
+    # else: use default functions that returns False as defaults
