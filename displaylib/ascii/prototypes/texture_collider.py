3f1efe2e7ec754222fae861cc09c7839db104440
Floating-Int <knutolaihelgesen@gmail.com>
2023-09-28 09:25:34 +0200

    Added type: ignore many places

--- displaylib/ascii/prototypes/texture_collider.py
+++ displaylib/ascii/prototypes/texture_collider.py
@@ -20,7 +20,7 @@ class TextCollider: # Component (mixin class)
         self.position += distance
         if collider := self.get_collider():
             self.position = old_position
-            self.position.y = collider._get_texture_global_position().y - self.size().y
+            self.position.y = collider._get_texture_global_position().y - self.size().y # type: ignore
     
     def move_and_slide(self, distance: Vec2) -> None:
         old_position = self.position.copy()
@@ -29,17 +29,17 @@ class TextCollider: # Component (mixin class)
         if collider := self.get_collider():
             self.position = old_position
             if signs.x == 1:
-                self.position.x = collider._get_texture_global_position().x - self.size().x
+                self.position.x = collider._get_texture_global_position().x - self.size().x # type: ignore
             elif signs.x == -1:
-                self.position.x = collider._get_texture_global_position().x + collider.size().x
+                self.position.x = collider._get_texture_global_position().x + collider.size().x # type: ignore
         old_position = self.position.copy()
         self.position.y += distance.y
         if collider := self.get_collider():
             self.position = old_position
             if signs.y == 1:
-                self.position.y = collider._get_texture_global_position().y - self.size().y
+                self.position.y = collider._get_texture_global_position().y - self.size().y # type: ignore
             elif signs.y == -1:
-                self.position.y = collider._get_texture_global_position().y + collider.size().y
+                self.position.y = collider._get_texture_global_position().y + collider.size().y # type: ignore
     
     def is_on_floor(self) -> bool:
         old_position = self.position.copy()
@@ -75,16 +75,16 @@ class TextCollider: # Component (mixin class)
     
     def is_colliding_with(self, other: TextCollider) -> bool:
         # basic implementation
-        position = self._get_texture_global_position()
-        start = other._get_texture_global_position()
-        rect = (start, start + other.size()) # start, end
+        position = self._get_texture_global_position() # type: ignore
+        start = other._get_texture_global_position() # type: ignore
+        rect = (start, start + other.size()) # start, end  # type: ignore
         return any( # any intersects
             self._point_intersects_with(point, rect)
             for point in [
                 position,
-                position + self.size(),
-                position + Vec2(self.size().x, 0),
-                position + Vec2(0, self.size().y)
+                position + self.size(), # type: ignore
+                position + Vec2(self.size().x, 0), # type: ignore
+                position + Vec2(0, self.size().y) # type: ignore
             ])
 
     def _point_intersects_with(self, point: Vec2, rect: tuple[Vec2, Vec2]) -> bool:
@@ -96,4 +96,4 @@ class TextCollider: # Component (mixin class)
     def queue_free(self) -> None:
         if self in TextCollider._colliders:
             TextCollider._colliders.remove(self)
-        super().queue_free()
+        super().queue_free() # type: ignore
