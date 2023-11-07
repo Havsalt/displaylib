019795ec615047f0cfbadc096489bcddd2600ab9
FloatingInt <knutolaihelgesen@gmail.com>
2023-11-07 19:21:23 +0100

    Added color module import

--- displaylib/pygame/__init__.py
+++ displaylib/pygame/__init__.py
@@ -13,6 +13,7 @@ __all__ = [
     "Vec2i",             # (data structure)
     # utility
     "autorun",           # (class decorator)
+    # "extend",            # (decorator)
     # base
     "BaseNode",          # (class alias)
     # core pygame
@@ -21,6 +22,8 @@ __all__ = [
     "Engine",            # (class)
     # mixin components
     "Transform2D",       # (component)
+    # generating colors
+    "color",             # (module)
     # networking
     "networking",        # (module)
     # typing support
@@ -34,13 +37,13 @@ try: # check if pygame is installed
     if not _pygame.get_init():
         _pygame.init()
     del _os, _pygame
-except ModuleNotFoundError as error:
-    raise ModuleNotFoundError("missing external module: pygame, which is required to use this submodule") from error
+except ModuleNotFoundError as _error:
+    raise ModuleNotFoundError("missing external module: pygame, which is required to use this submodule") from _error
 
 # math
 from ..math import lerp, sign, clamp, Vec2, Vec2i
 # utility
-from ..util import autorun
+from ..util import autorun#, extend
 # base
 from ..template import Node as BaseNode
 # core pygame
@@ -48,6 +51,8 @@ from .node import PygameNode as Node, PygameNode2D as Node2D
 from .engine import PygameEngine as Engine
 # mixin components
 from ..template import Transform2D
+# generating colors
+from . import color
 # networking
 from ..template import networking
 # typing support
