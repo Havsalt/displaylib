9450f8a34af93c828214545d596d6919c641c60e
FloatingInt <knutolaihelgesen@gmail.com>
2023-11-20 19:20:12 +0100

    Added 'speed' member, reimporting as local namespaces

--- displaylib/ascii/prototypes/simple_movement.py
+++ displaylib/ascii/prototypes/simple_movement.py
@@ -2,6 +2,7 @@ from __future__ import annotations as _annotations
 
 from typing import TYPE_CHECKING as _TYPE_CHECKING
 
+from ...math import Vec2 as _Vec2
 from ...template.type_hints import NodeType as _NodeType
 from .. import keyboard as _keyboard
 
@@ -10,11 +11,16 @@ if _TYPE_CHECKING:
     from ...template.type_hints import UpdateFunction as _UpdateFunction, ValidTransform2DNode as _ValidTransform2DNode
     
     class _ValidSimpleMovement2DNode(_ValidTransform2DNode, _Protocol):
-        def _simple_movement_2d_update_wrapper(self: _ValidSimpleMovement2DNode, update_function: _UpdateFunction) -> _UpdateFunction:
-            ...
+        def _simple_movement_2d_update_wrapper(self: _ValidSimpleMovement2DNode, update_function: _UpdateFunction) -> _UpdateFunction: ...
+        @property
+        def speed(self) -> _Vec2: ...
+        @speed.setter
+        def speed(self, value: _Vec2) -> None: ...
 
 
 class SimpleMovement2D: # Component (mixin class)
+    speed: _Vec2 = _Vec2.ZERO
+
     def __new__(cls: type[_NodeType], *args, **kwargs) -> _NodeType:
         instance = super().__new__(cls, *args, **kwargs) # type: _ValidSimpleMovement2DNode  # type: ignore
         instance._update = instance._simple_movement_2d_update_wrapper(instance._update) # type: ignore
@@ -23,12 +29,12 @@ class SimpleMovement2D: # Component (mixin class)
     def _simple_movement_2d_update_wrapper(self: _ValidSimpleMovement2DNode, update_function: _UpdateFunction) -> _UpdateFunction:
         def _update(delta: float):
             if _keyboard.is_pressed("D"):
-                self.position.x += 1
+                self.position.x += self.speed.x
             if _keyboard.is_pressed("A"):
-                self.position.x -= 1
+                self.position.x -= self.speed.x
             if _keyboard.is_pressed("W"):
-                self.position.y -= 1
+                self.position.y -= self.speed.x
             if _keyboard.is_pressed("S"):
-                self.position.y += 1
+                self.position.y += self.speed.x
             update_function(delta)
         return _update
